148d464bbcf67ca0be546dffb54dd4ede925bdc2
Geoffrey <MLGSTUN@gmail.com>
2023-04-21 17:54:58 -0400

    xxxxxxxxxxxxxx

--- Home Page.php
+++ Home Page.php
@@ -6,13 +6,42 @@ require_once 'functions.php'; // File containing functions for retrieving data f
 // Retrieve clients data from the database
 $clients = getClients();
 
-// Fetch spot prices of BTC, ETH, LTC, and top 10 cryptocurrencies from an API
-$apiUrl = 'https://api.example.com/prices'; // Replace with the actual API URL
-$cryptoList = array('BTC', 'ETH', 'LTC'); // List of cryptocurrencies to fetch
-$topCryptos = array('BTC', 'ETH', 'LTC'); // Top cryptocurrencies to display
+// Fetch spot prices of BTC, ETH, LTC, and top 10 cryptocurrencies from Yahoo Finance API
+$apiUrl = 'https://query1.finance.yahoo.com/v7/finance/quote'; // Yahoo Finance API URL
+$cryptoList = array('BTC-USD', 'ETH-USD', 'LTC-USD'); // List of cryptocurrencies to fetch
 $cryptos = fetchCryptocurrencyPrices($apiUrl, $cryptoList); // Function to fetch cryptocurrency prices from the API
-?>
 
+if ($cryptos === false) { // Check if there was an error fetching the cryptocurrency prices
+    echo '<p>Error fetching cryptocurrency prices from the API. Please try again later.</p>';
+}
+
+// Fetch market data for the top cryptocurrencies from Yahoo Finance API
+$yahooURL = 'https://query1.finance.yahoo.com/v7/finance/quote?'; // Yahoo Finance API URL
+$topCryptos = array('BTC', 'ETH', 'LTC'); // Top cryptocurrencies to display
+$symbols = implode(',', $topCryptos); // Comma-separated list of symbols
+$params = array(
+    'symbols' => $symbols,
+    'fields' => 'regularMarketPrice,regularMarketChangePercent,regularMarketVolume',
+);
+$marketData = fetchMarketData($yahooURL, $params); // Function to fetch market data from Yahoo Finance API
+
+if ($marketData === false) { // Check if there was an error fetching the market data
+    echo '<p>Error fetching market data from the API. Please try again later.</p>';
+}
+
+// Sanitize market data
+foreach ($marketData as &$marketDatum) {
+    foreach ($marketDatum as &$datum) {
+        $datum = filter_var($datum, FILTER_SANITIZE_STRING);
+    }
+}
+
+// Display error message if market data is not available
+if (empty($marketData)) {
+    echo '<p>Market data is not available at the moment. Please try again later.</p>';
+}
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -40,99 +69,46 @@ $cryptos = fetchCryptocurrencyPrices($apiUrl, $cryptoList); // Function to fetch
             </ul>
         </nav>
     </header>
-    
     <!-- Main content section -->
-    <main>
-        <section>
-            <h2>Our Clients</h2>
-            <p>GG Holdings Group has had the privilege of serving a wide range of clients in various industries. Our clients include:</p>
-            <ul>
-                <?php if (!empty($clients)) { // Use empty() function to check if $clients is not empty
-                    foreach ($clients as $client) {
-                        echo '<li>' . $client['client_name'] . '</li>';
-                    }
-                } else {
-                    echo '<li>No clients found.</li>';
-                }
-                ?>
-            </ul>
-        </section>
-        
-        <section>
-            <h2>Cryptocurrency Prices</h2>
-            <table>
-                <tr>
-                    <th>Cryptocurrency</th>
-                    <th>Price (USD)</th>
-                </tr>
-                <?php
-                foreach ($topCryptos as $crypto) {
-                    if (isset($cryptos[$crypto])) { // Use isset() function to check if $cryptos[$crypto] is set
-                        echo '<tr>';
-                        echo '<td>' . $crypto . '</td>';
-                        echo '<td>' . $cryptos[$crypto] . '</td>';
-                        echo '</tr>';
-                    }
-                }
-                <section>
-    <h2>Cryptocurrency Prices</h2>
-    <table>
-        <tr>
-            <th>Cryptocurrency</th>
-            <th>Price (USD)</th>
-            <th>US Market Data</th>
-        </tr>
-        <?php
-        foreach ($topCryptosWithUSMarket as $crypto) {
-            echo '<tr>';
-            echo '<td>' . $crypto . '</td>';
-            echo '<td>' . $cryptos[$crypto] . '</td>';
-            // Fetch and display US market data for the cryptocurrency
-            $usMarketData = fetchUSMarketData($usMarketUrl, $crypto); // Function to fetch US market data from the API
-            echo '<td>' . $usMarketData . '</td>';
-            echo '</tr>';
-        }
-        ?>
-    </table>
-</section>
+<main>
+    <section>
+        <h2>Our Clients</h2>
+        <p>GG Holdings Group has had the privilege of serving a wide range of small to midsize companies in various industries. Our clients include businesses in tech, finance, retail, healthcare, and more. We are proud to have earned their trust and to have played a role in their growth and success.</p>
+        <ul>
+            <?php if (!empty($clients)) { // Use empty() function
+<div class="data-container">
+<?php foreach ($clients as $client): ?>
+    <div><?= $client['name'] ?> - <?= $client['industry'] ?></div>
+<?php endforeach; ?>
+</div>
 
-<section>
-    <h2>US Market Data</h2>
-    <div class="us-market-box">
-        <h3>QQQ</h3>
-        <?php
-        // Fetch and display QQQ data
-        $qqqData = fetchUSMarketData($usMarketUrl, 'QQQ'); // Function to fetch US market data for QQQ from the API
-        echo '<p>' . $qqqData . '</p>';
-        ?>
-    </div>
-    <div class="us-market-box">
-        <h3>SPY</h3>
-        <?php
-        // Fetch and display SPY data
-        $spyData = fetchUSMarketData($usMarketUrl, 'SPY'); // Function to fetch US market data for SPY from the API
-        echo '<p>' . $spyData . '</p>';
-        ?>
-    </div>
-    <div class="us-market-box">
-        <h3>IWM</h3>
-        <?php
-        // Fetch and display IWM data
-        $iwmData = fetchUSMarketData($usMarketUrl, 'IWM'); // Function to fetch US market data for IWM from the API
-        echo '<p>' . $iwmData . '</p>';
-        ?>
-    </div>
-</section>
-<p> OUR LOGISTICS WEBSITE: https://www.GGDataGroup.com 
+// Include the GeoIP2 PHP API
+require_once 'vendor/autoload.php';
 
-                ?>
-            </table>
-        </section>
-    </main>
-    
-    <!-- Footer section -->
-    <footer>
-        <p>Contact us: ggeltman@ggdatagroup.com </p>
-        <!-- Add any additional footer content as needed -->
-        <ul class="social-media">
-            <li><a href="https://www.facebook.com/ggholdings" target="_blank"><i class="fab fa-facebook"></i></a
+use GeoIp2\Database\Reader;
+
+// Function to fetch geo data for the user accessing the website
+function fetchGeoData() {
+    // Initialize the GeoIP2 database reader with the path to the GeoLite2-City.mmdb file
+    $reader = new Reader('path/to/GeoLite2-City.mmdb');
+
+    // Get the IP address of the user accessing the website
+    $ipAddress = $_SERVER['REMOTE_ADDR'];
+
+    // Fetch the geo data for the user's IP address
+    try {
+        $record = $reader->city($ipAddress);
+    } catch (\Exception $e) {
+        // If an error occurs, return false
+        return false;
+    }
+
+    // Return the geo data as an array
+    return array(
+        'country' => $record->country->name,
+        'region' => $record->mostSpecificSubdivision->name,
+        'city' => $record->city->name,
+        'latitude' => $record->location->latitude,
+        'longitude' => $record->location->longitude,
+    );
+}
